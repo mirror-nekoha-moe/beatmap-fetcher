4a48d170761c89bbd044282b15fcc5a94b5fab8b
nyaruku <38096703+nyaruku@users.noreply.github.com>
2025-12-29 02:57:33 +0100

    BeatmapController: directly pass rawBeatmap object to insertBeatmap function

--- src/Domain/Beatmap/Controller/BeatmapController.ts
+++ src/Domain/Beatmap/Controller/BeatmapController.ts
@@ -3,11 +3,7 @@ import { BeatmapRepository } from "@Domain/Beatmap/Repository/BeatmapRepository"
 export class BeatmapController {
     static async processBeatmap(rawBeatmapset: any) {
         for (const rawBeatmap of rawBeatmapset.beatmaps) {
-            const mappedBeatmap = {
-                ...rawBeatmap,
-                creator: rawBeatmapset.creator, // Add beatmapset creator to beatmap
-            };            
-            await BeatmapRepository.insertBeatmap(mappedBeatmap);
+            await BeatmapRepository.insertBeatmap(rawBeatmap);
         }
     }
 }
